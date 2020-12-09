e1053f0bf74877a3cb96becb2dfb69dca3fd5007
Sami Mustonen <bonekuukkeli@hotmail.com>
2020-12-09 09:27:39 +0200

    Folder/task comments added for Day 1

--- Day1/Day1HA2.js
+++ Day1/Day1HA2.js
@@ -1,3 +1,5 @@
+// *** Create a program that takes in a number from command line that represents a length of a squares sides. Calculate the area of the square with given number.
+
 const sideA = Number(process.argv[2]);
 const sideB = Number(process.argv[3]);
 
