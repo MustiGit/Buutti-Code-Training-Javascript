e34f66a4a11e3a47f9670b09a1c7cdbbe3569e92
Sami Mustonen <bonekuukkeli@hotmail.com>
2021-01-13 10:02:42 +0200

    Added Day2 and first lecture

---
+++ Week4/Day2/Lectures/countDown.js
@@ -0,0 +1,12 @@
+anotherFunction(1000, () => {
+    console.log("first");
+    anotherFunction(2000, () => {
+        console.log("second");
+        anotherFunction(500, () => {
+            console.log("third");
+            anotherFunction(1500, () => {
+                console.log("oh jesus...");
+            });
+        });
+    });
+});
