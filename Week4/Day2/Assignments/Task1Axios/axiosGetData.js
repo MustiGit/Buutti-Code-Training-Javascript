854e37e10c38346501dc873a526927c6acbfb07c
Sami Mustonen <bonekuukkeli@hotmail.com>
2021-01-18 08:44:07 +0200

    Task 1 somewhat done

--- Week4/Day2/Assignments/Task1Axios/axiosGetData.js
+++ Week4/Day2/Assignments/Task1Axios/axiosGetData.js
@@ -1,40 +1,44 @@
 import axios from "axios";
 
+const func = async function(url) {
+    // Get url data to resp
+    const resp = await axios.get(url);
 
-const func = async () => {
-    const resp = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    // The !Array.isArray(resp.data) method determines whether resp.data is NOT an array.
+    if (!Array.isArray(resp.data)) {
+        // If it's not an array, make it array
+        resp.data = [resp.data];
+    }
 
-    const respUser = await axios.get(`https://jsonplaceholder.typicode.com/users/${resp.data.userId}`);
+    // Get users data to user
+    const users = await axios.get("https://jsonplaceholder.typicode.com/users/");
 
-    const todoObject = {
-        id: resp.data.id,
-        title: resp.data.title,
-        completed: resp.data.completed,
-        user: respUser.data,
-    };
-    console.log(todoObject);
+    // Map through resp.data
+    const modifiedData = resp.data.map((respElement) => {
+        if (respElement.userId) {
+            const userData = idCompare(users, respElement);
+
+            const todoObject = {
+                id: respElement.id,
+                title: respElement.title,
+                completed: respElement.completed,
+                user: {
+                    // Modify user to only show name, username and email
+                    name: userData.name,
+                    username: userData.username,
+                    email: userData.email,
+                },
+            };
+            return todoObject;
+        }
+        return respElement;
+    });
+
+    console.log(modifiedData);
 };
 
-func();
+func("https://jsonplaceholder.typicode.com/todos/");
 
-
-/*
-const url1 = "https://jsonplaceholder.typicode.com/todos/";
-const url2 = "https://jsonplaceholder.typicode.com/todos/1";
-
-const func = async (url) => {
-    const resp = await axios.get(url2);
-
-    const respUser = await axios.get(url1);
-
-    const todoObject = {
-        name: resp.data.name,
-        username: resp.data.username,
-        email: resp.data.email,
-        user: respUser.data,
-    };
-    console.log(todoObject);
-};
-
-func();
-*/
+function idCompare(users, respElement) {
+    return users.data.find((usersElement) => respElement.userId === usersElement.id);
+}
